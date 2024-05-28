337010555c3111c56106ce7726d3a4101e7138ae
arefsa78 <arefsayareh@gmail.com>
2024-05-27 23:06:22 -0230

    init

---
+++ receiver.py
@@ -0,0 +1,34 @@
+import sys
+import time
+import pika
+
+class Receiver:
+    def __init__(self, name):
+        self.name = name
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='test')
+        self.channel.queue_declare(queue='test_reply')
+    
+    def callback(self, ch, method, properties, body):
+        self.consume(body.decode())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='test_reply',
+                                   body=f'{body},{self.name}')
+    
+    def receive(self):
+        self.channel.basic_consume(queue='test',
+                        on_message_callback=self.callback,
+                        auto_ack=True)
+        print("RECEIVING...")
+        self.channel.start_consuming()
+        print("DONE")
+        
+    def consume(self, body):
+        sleep_time = int(body.split(',')[3])
+        print(f"RECEIVER {self.name} got: {body}")
+        time.sleep(sleep_time)
+        
+name = sys.argv[1]
+receiver = Receiver(name)
+receiver.receive()
---
+++ sender.py
@@ -0,0 +1,42 @@
+import random
+import time
+import pika
+
+class Sender:
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='test')
+        self.channel.queue_declare(queue='test_reply')
+        self.log = open('sender.log', 'w')
+    
+    def send(self, message):
+        self.channel.basic_publish(exchange='',
+                        routing_key='test',
+                        body=message)
+        print(f" [x] Sent {message}")
+    
+    def close(self):
+        self.connection.close()
+        
+    def produce(self):
+        for i in range(100):
+            self.send(f"M,{i},S,{random.randint(0, 10)}")
+    
+    def receive(self):
+        self.channel.basic_consume(queue='test_reply',
+                        on_message_callback=self.callback,
+                        auto_ack=True)
+        print("RECEIVING...")
+        self.channel.start_consuming()
+        print("DONE")
+    
+    def callback(self, ch, method, properties, body):
+        body = body.decode()
+        print(f" [x] Received {body}")
+        self.log.write(f"{body} \n")
+        self.log.flush()
+
+sender = Sender()
+sender.produce()
+sender.receive()
